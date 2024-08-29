ab4b37992535165f2063d86da674bceddbd903e1
consoom <33983173+consoom@users.noreply.github.com>
2024-08-30 00:02:19 +0200

    Remove unnecessary line

--- x.c
+++ x.c
@@ -298,8 +298,7 @@ numlock(const Arg *dummy)
 void
 changealpha(const Arg *arg)
 {
-    if((alpha > 0 && arg->f < 0) || (alpha < 1 && arg->f > 0))
-        alpha += arg->f;
+    alpha += arg->f;
     if(alpha < 0)
         alpha = 0;
     if(alpha > 1)

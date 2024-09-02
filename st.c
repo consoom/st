19f64e40404db7c928ca393e949930504afb22f8
consoom <33983173+consoom@users.noreply.github.com>
2024-09-02 18:02:56 +0200

    Added URL highlighting/opening with ctrl

--- st.c
+++ st.c
@@ -651,6 +651,101 @@ getsel(void)
 	return str;
 }
 
+char *
+strstrany(char* s, char** strs) {
+	char *match;
+	for (int i = 0; strs[i]; i++) {
+		if ((match = strstr(s, strs[i]))) {
+			return match;
+		}
+	}
+	return NULL;
+}
+
+void
+highlighturls(void)
+{
+	char *match;
+	char *linestr = calloc(sizeof(char), term.col+1); /* assume ascii */
+	int y = TLINEOFFSET(0);
+	for (int i = term.top; i < term.bot; i++) {
+		Line line = TSCREEN.buffer[y+i];
+		int url_start = -1;
+		for (int j = 0; j < term.col; j++) {
+			if (line[j].u < 127) {
+				linestr[j] = line[j].u;
+			}
+			linestr[term.col] = '\0';
+		}
+		while ((match = strstrany(linestr + url_start + 1, urlprefixes))) {
+			url_start = match - linestr;
+			for (int c = url_start; c < term.col && strchr(urlchars, linestr[c]); c++) {
+				line[c].mode |= ATTR_URL;
+				tsetdirt(i, c);
+			}
+		}
+	}
+	free(linestr);
+}
+
+void
+unhighlighturls(void)
+{
+	int y = TLINEOFFSET(0);
+	for (int i = term.top; i < term.bot; i++) {
+		Line line = TSCREEN.buffer[y+i];
+		for (int j = 0; j < term.col; j++) {
+			Glyph* g = &line[j];
+			if (g->mode & ATTR_URL) {
+				g->mode &= ~ATTR_URL;
+				tsetdirt(i, j);
+			}
+		}
+	}
+	return;
+}
+
+void
+followurl(int x, int y) {
+	char *linestr = calloc(sizeof(char), term.col+1); /* assume ascii */
+	int ya = TLINEOFFSET(x);
+	Line line = TSCREEN.buffer[ya];
+	char *match;
+	for (int i = 0; i < term.col; i++) {
+		if (line[i].u < 127) {
+			linestr[i] = line[i].u;
+		}
+		linestr[term.col] = '\0';
+	}
+	int url_start = -1;
+	while ((match = strstrany(linestr + url_start + 1, urlprefixes))) {
+		url_start = match - linestr;
+		int url_end = url_start;
+		for (int c = url_start; c < term.col && strchr(urlchars, linestr[c]); c++) {
+			url_end++;
+		}
+		if (url_start <= y && y < url_end) {
+			linestr[url_end] = '\0';
+			break;
+		}
+	}
+	if (url_start == -1) {
+		free(linestr);
+		return;
+	}
+
+	pid_t chpid;
+	if ((chpid = fork()) == 0) {
+		if (fork() == 0)
+			execlp(urlhandler, urlhandler, linestr + url_start, NULL);
+		exit(1);
+	}
+	if (chpid > 0)
+		waitpid(chpid, NULL, 0);
+	free(linestr);
+	unhighlighturls();
+}
+
 void
 selclear(void)
 {
